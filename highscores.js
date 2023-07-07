2fd4e92cb507ede49b26c1282be85537b368a85c
GustavoKich001 <131195081+GustavoKich001@users.noreply.github.com>
2023-07-07 10:39:48 -0300

    Update highscores.js

--- highscores.js
+++ highscores.js
@@ -1,6 +1,18 @@
+
 const highScoresList = document.querySelector('#highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-highScoresList.innerHTML = highScores.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-}).join('');
+highScoresList.innerHTML = highScores
+  .map((score, index) => {
+    return `<li class="high-score">${index + 1}. ${score.name} - ${score.score}</li>`;
+  })
+  .join('');
+
+const clearScoresBtn = document.querySelector('#clearScoresBtn');
+
+clearScoresBtn.addEventListener('click', clearHighScores);
+
+function clearHighScores() {
+  localStorage.removeItem('highScores');
+  highScoresList.innerHTML = '';
+};
